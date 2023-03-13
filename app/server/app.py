3294c4d4d2631a33cab7d5babfb91d8d7b770032
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-03-13 11:45:57 +0530

    Login structure changes

--- app/server/app.py
+++ app/server/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from server.models.user import UserSchema
 from server.routes.project import router as ProjectRouter
+from server.helper.login import *
 
 app = FastAPI()
 app.include_router(ProjectRouter, tags=["Project"], prefix="/project")
@@ -11,51 +12,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 @app.get("/", tags=["Root"])
 async def read_root(token: str = Depends(oauth2_scheme)):
     return {"message":"Welcome to Constuction Cost estimator", "token": token}
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
-
-def fake_hashed_password(password: str):
-    return "fakehashed" + password
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserSchema(**user_dict)
-
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
-    return user
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate":"Bearer"},
-        )
-    return user
-
-async def get_current_active_user(current_user: UserSchema = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
